be86e33903a097e6523f74df91d5d2b66732fe1b
VanshikaRathore <96684855+VanshikaRathore@users.noreply.github.com>
2024-07-08 09:31:11 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14453
+let accountEmail = "vanshika@ghn.com"
+var accountPassword="2345"
+
+accountCity="Indore"
+// accountId = 2345 // not allowed in const keyword value not changed
+console.log(accountId)
+
+accountEmail ="hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+console.table([accountId,accountEmail,accountPassword, accountCity])
