11c09235f8ddc0908d085ae8aa8a8415baed89f8
VanshikaRathore <96684855+VanshikaRathore@users.noreply.github.com>
2024-07-08 10:55:02 +0000

    Conversion of Datatypes

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -15,3 +15,19 @@ let isLoggedIn = 0;
  
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
+
+let number = 35
+ let StringName = String(number)
+ console.log( typeof StringName);
+
+ // **************************Operation*****************************
+
+
+ console.log(2 + '3')
+ console.log('3' + 4)
+ console.log(1 + 2 +"3")
+ console.log("3" + 2 +9)
+
+ let gameCounter = 100
+ ++gameCounter;
+ console.log(gameCounter);
