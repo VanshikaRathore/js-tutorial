a4f3e80642d6350e1a2dc9e3878b00423f72c2cd
VanshikaRathore <96684855+VanshikaRathore@users.noreply.github.com>
2024-07-08 10:02:30 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,17 @@
+let score = "33"
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueIntNumber = Number(score)
+console.log(typeof valueIntNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1
+//False => 0
+ 
+let isLoggedIn = 0;
+ 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
