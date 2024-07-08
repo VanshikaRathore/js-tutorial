d9174f8b7e5ac768a71c19d2bc481942f3206bb5
VanshikaRathore <96684855+VanshikaRathore@users.noreply.github.com>
2024-07-08 09:26:42 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3)// we are using nodejs not browser
+
+
+/* primitive data types */
+//number = 2 to power 53
+//string=""
+//bigint
+//boolean= true/false
+//null= standalone value
+//undefined
+//symbol = unique
+
