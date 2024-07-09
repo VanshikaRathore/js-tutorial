557f96b54b05b99e89e8db6aca4e420f48276c0e
VanshikaRathore <96684855+VanshikaRathore@users.noreply.github.com>
2024-07-09 09:47:57 +0000

    Nums & Maths

---
+++ 01_basics/07_nums_and_math.js
@@ -0,0 +1,36 @@
+// const balance = 400;
+// console.log(balance);
+
+// const score = new Number(100);
+// console.log(score);
+
+// console.log(score.toString().length);
+// console.log(score.toFixed(3));
+
+// const otherNumber = 2223.4565;
+// console.log(otherNumber.toPrecision(3));
+
+// const hundereds = 100000000;
+// console.log(hundereds.toLocaleString('en-IN'));
+
+
+/***************************Maths******************************************* */
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.51));
+// console.log(Math.ceil(6.2));
+// console.log(Math.floor(5.9));
+// console.log(Math.min(2,3,5,7,7));
+// console.log(Math.max(5,4,8,7,7,8));
+
+
+console.log(Math.random());
+
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.random());
+ console.log(Math.floor(Math.random()*(max-min+1)) + min)
