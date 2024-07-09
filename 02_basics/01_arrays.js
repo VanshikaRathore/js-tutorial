43c5ef23bf1ee4522895e757f82d0bf7098e8a15
VanshikaRathore <96684855+VanshikaRathore@users.noreply.github.com>
2024-07-09 12:37:40 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+// const colors = ["red", "yellow", "blue"];
+// colors[4]="pink";
+// colors[5] = "purple";
+// colors.forEach((item, index) => {
+//   console.log(`${index}: ${item}`);
+// });
+
+// colors.reverse(); 
+// console.log(colors);
+
+const myArr =[0,1,2,3,4,5];
+const myHeros =["shakiman" , "naagraj"];
+
+const myArr1 = new Array(1,2,3,4)
+console.log(myArr1[2]);
+
+myArr.push(8);
+console.log(myArr);
+
+myArr.pop(8);
+console.log(myArr);
+myArr.unshift(0);
+console.log(myArr);
+myArr.shift(2);
+myArr.shift(0);
+console.log(myArr);
