de13f860dc00533cab611d53df7f6dba93fbce27
VanshikaRathore <96684855+VanshikaRathore@users.noreply.github.com>
2024-07-17 11:20:32 +0000

    GuessThe Num

--- GuessTheNum/script.js
+++ GuessTheNum/script.js
@@ -15,5 +15,75 @@ let numGuess = 1
 
 let playGame = true;
 
+if(playGame){
+   submit.addEventListener('click',function(e){
+    e.preventDefault();
+    const guess = parseInt(userInput.value);
+    console.log(guess);
+    validateGuess(guess);
+   });
+}
 
+function validateGuess(guess){
+ if(isNaN(guess)){
+    alert('please enter a valid number')
+ }else if(guess < 1){
+    alert('please enter a number more than 1')
+ }else if(guess > 100){
+    alert('please enter a number less than 100 ')
+ }else{
+    prevGuess.push(guess)
+    if(numGuess === 1){
+        displayGuess(guess)
+        displayMessage('Game Over. random number was ${radomNumber}')
+        endGame()
+    }else{
+        displayGuess(guess)
+        checkGuess(guess)
+    }
+}
+}
+function checkGuess(guess){
+   if(guess === randomNumber){
+    displayMessage('You guessed it right')
+    endGame()
+   }else if(guess < randomNumber){
+       displayMessage('Number is Tooo low')
+   }else if(guess > randomNumber){ 
+        displayMessage('Number is Too High')
+   }
+}
+
+function displayGuess(guess){
+   userInput.value = ''
+   guessSlot.innerHTML += `${guess}  `;
+   numGuess++;
+   remaining.innerHTML = `${11-numGuess} `;
+}
+function displayMessage(message){
+   lowOrHigh.innerHTML = `<h2>${message}</h2>`;
+}
+function endGame(){
+    userInput.value = ''
+    userInput.setAttribute('disabled','')
+    p.classList.add('button')
+    p.innerHTML = `<h2 id = "nreGame">Start New Game</h2>`;
+    startOver.appendChild(p);
+    playGame=false;
+    newGame();
+}
+function newGame(){
+   const newGameButton = document.querySelector('#newGame')
+   newGameButton.addEventListener('click', function(e){
+      randomNumber = parseInt(Math.random()*100 + 1);
+      prevGuess = [];
+      newGuess = 1;
+      guessSlot.innerHTMl = "";
+      remaining.innerHTML = `${11-numGuess} `;
+      userInput.removeAttribute('disabled');
+      startOver.removeChild(p);
+
+      playGame = true;
+   })
+}
 
