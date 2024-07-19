3c58033c14d24ea8a883853895af3eeb031201ed
VanshikaRathore <96684855+VanshikaRathore@users.noreply.github.com>
2024-07-19 08:28:49 +0000

    Oops

---
+++ 10_Oops/classess.js
@@ -0,0 +1,20 @@
+class User{
+    constructor(username,email, password)
+    {
+        this.username = username;
+        this.email = email;
+        this.password = password
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+
+const name = new User('vanshi', 'rathorevanshigmail.com','123')
+
+console.log(name.encryptPassword());
+console.log(name.changeUsername());
